6fe128e5379159a39c39ab59cd83394e4d69d091
sruthiviswanathan <sravan1097@gmail.com>
2021-01-19 14:44:02 +0530

    Database initialization with spring.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT into category (description) VALUES ('American');
+INSERT into category (description) VALUES ('Italian');
+INSERT into category (description) VALUES ('Chinese');
+INSERT into category (description) VALUES ('Fast food');
+
+INSERT into unit_of_measure (description) VALUES ('Teaspoon');
+INSERT into unit_of_measure (description) VALUES ('Tablespoon');
+INSERT into unit_of_measure (description) VALUES ('cup');
+INSERT into unit_of_measure (description) VALUES ('pinch');
+INSERT into unit_of_measure (description) VALUES ('as required');
